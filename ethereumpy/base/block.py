4319aba595c15b4380414259d67214fb15e4b7af
dongchangYoo <dongchang.yoo@pi-lab.io>
2021-08-19 18:49:38 +0900

    add getters for every member of a block

--- ethereumpy/base/block.py
+++ ethereumpy/base/block.py
@@ -1,3 +1,4 @@
+from ethereumpy.base.transaction import EthTransaction
 from ethereumpy.type.eth_hexstring import EthHexString, EthHashString
 from ethereumpy.type.eth_address import ChecksumAddress
 from unittest import TestCase
@@ -12,27 +13,27 @@ class EthBlock:
             size: int, state_root: EthHashString, timestamp: int, total_difficulty: int,
             transactions_root: EthHashString, transactions: list, uncles: list):
 
-        self.base_fee_per_gas: int = base_fee_per_gas
-        self.difficulty: int = difficulty
-        self.extra_data: EthHexString = extra_data
-        self.gas_limit: int = gas_limit
-        self.gas_used: int = gas_used
-        self.hash_: EthHashString = hash_
-        self.logs_bloom: EthHexString = logs_bloom
-        self.miner: ChecksumAddress = miner
-        self.mix_hash: EthHashString = mix_hash
-        self.nonce: int = nonce
-        self.number: int = number
-        self.parent_hash: EthHashString = parent_hash
-        self.receipts_root: EthHashString = receipts_root
-        self.sha3_uncles: EthHashString = sha3_uncles
-        self.size: int = size
-        self.state_root: EthHashString = state_root
-        self.timestamp: int = timestamp
-        self.total_difficulty: int = total_difficulty
-        self.transactions_root: EthHashString = transactions_root
-        self.transactions: list = transactions
-        self.uncles: list = uncles
+        self._base_fee_per_gas: int = base_fee_per_gas
+        self._difficulty: int = difficulty
+        self._extra_data: EthHexString = extra_data
+        self._gas_limit: int = gas_limit
+        self._gas_used: int = gas_used
+        self._hash: EthHashString = hash_
+        self._logs_bloom: EthHexString = logs_bloom
+        self._miner: ChecksumAddress = miner
+        self._mix_hash: EthHashString = mix_hash
+        self._nonce: int = nonce
+        self._number: int = number
+        self._parent_hash: EthHashString = parent_hash
+        self._receipts_root: EthHashString = receipts_root
+        self._sha3_uncles: EthHashString = sha3_uncles
+        self._size: int = size
+        self._state_root: EthHashString = state_root
+        self._timestamp: int = timestamp
+        self._total_difficulty: int = total_difficulty
+        self._transactions_root: EthHashString = transactions_root
+        self._transactions: list = transactions
+        self._uncles: list = uncles
 
     @classmethod
     def from_dict(cls, block_dict: dict):
@@ -55,7 +56,7 @@ class EthBlock:
         timestamp: int = int(block_dict["timestamp"], 16)
         total_difficulty: int = int(block_dict["totalDifficulty"], 16)
         transactions_root: EthHashString = EthHashString.from_hex(block_dict["transactionsRoot"])
-        transactions: list = block_dict["transactions"]
+        transactions: list = [EthTransaction.from_dict(tx) for tx in block_dict["transactions"]]
         uncles: list = block_dict["uncles"]
 
         return cls(base_fee_per_gas, difficulty, extra_data, gas_limit, gas_used, hash_, logs_bloom, miner, mix_hash,
@@ -64,36 +65,126 @@ class EthBlock:
 
     def to_dict(self):
         ret_dict = dict()
-        ret_dict["baseFeePerGas"] = hex(self.base_fee_per_gas)
-        ret_dict["difficulty"] = hex(self.difficulty)
-        ret_dict["extraData"] = self.extra_data.to_string_with_0x()
-        ret_dict["gasLimit"] = hex(self.gas_limit)
-        ret_dict["gasUsed"] = hex(self.gas_used)
-        ret_dict["hash"] = self.hash_.to_string_with_0x()
-        ret_dict["logsBloom"] = self.logs_bloom.to_string_with_0x()
-        ret_dict["miner"] = self.miner.to_string_with_0x()
-        ret_dict["mixHash"] = self.mix_hash.to_string_with_0x()
-        ret_dict["nonce"] = hex(self.nonce)
-        ret_dict["number"] = hex(self.number)
-        ret_dict["parentHash"] = self.parent_hash.to_string_with_0x()
-        ret_dict["receiptsRoot"] = self.receipts_root.to_string_with_0x()
-        ret_dict["sha3Uncles"] = self.sha3_uncles.to_string_with_0x()
-        ret_dict["size"] = hex(self.size)
-        ret_dict["stateRoot"] = self.state_root.to_string_with_0x()
-        ret_dict["timestamp"] = hex(self.timestamp)
-        ret_dict["totalDifficulty"] = hex(self.total_difficulty)
-        ret_dict["transactionsRoot"] = self.transactions_root.to_string_with_0x()
+        ret_dict["baseFeePerGas"] = hex(self._base_fee_per_gas)
+        ret_dict["difficulty"] = hex(self._difficulty)
+        ret_dict["extraData"] = self._extra_data.to_string_with_0x()
+        ret_dict["gasLimit"] = hex(self._gas_limit)
+        ret_dict["gasUsed"] = hex(self._gas_used)
+        ret_dict["hash"] = self._hash.to_string_with_0x()
+        ret_dict["logsBloom"] = self._logs_bloom.to_string_with_0x()
+        ret_dict["miner"] = self._miner.to_string_with_0x()
+        ret_dict["mixHash"] = self._mix_hash.to_string_with_0x()
+        ret_dict["nonce"] = hex(self._nonce)
+        ret_dict["number"] = hex(self._number)
+        ret_dict["parentHash"] = self._parent_hash.to_string_with_0x()
+        ret_dict["receiptsRoot"] = self._receipts_root.to_string_with_0x()
+        ret_dict["sha3Uncles"] = self._sha3_uncles.to_string_with_0x()
+        ret_dict["size"] = hex(self._size)
+        ret_dict["stateRoot"] = self._state_root.to_string_with_0x()
+        ret_dict["timestamp"] = hex(self._timestamp)
+        ret_dict["totalDifficulty"] = hex(self._total_difficulty)
+        ret_dict["transactionsRoot"] = self._transactions_root.to_string_with_0x()
 
-        ret_dict["transactions"] = self.transactions  # TODO change list of dict to object list
-        ret_dict["uncles"] = self.uncles  # TODO change list of dict to object list
+        ret_dict["transactions"] = [tx.to_dict() for tx in self._transactions]
+        ret_dict["uncles"] = self._uncles  # TODO change list of dict to object list
         return ret_dict
 
     def serialize(self):
+        # TODO hash value of the "serialize()" output must equals to block hash
+        pass
+
+    def self_verify(self):
+        # TODO verification of 1) transaction root, 2) condition of the block hash and etc
         pass
 
     @property
-    def hash(self):
-        return None
+    def base_fee(self) -> str:
+        return hex(self._base_fee_per_gas)
+
+    @property
+    def difficulty(self) -> str:
+        return hex(self._difficulty)
+
+    @property
+    def extra_data(self) -> str:
+        return self._extra_data.to_string_with_0x()
+
+    @property
+    def gas_limit(self) -> str:
+        return hex(self._gas_limit)
+
+    @property
+    def gas_used(self) -> str:
+        return hex(self._gas_used)
+
+    @property
+    def hash(self) -> str:
+        return self._hash.to_string_with_0x()
+
+    @property
+    def logs_bloom(self) -> str:
+        return self._logs_bloom.to_string_with_0x()
+    @property
+    def miner(self) -> str:
+        return self._miner.to_string_with_0x()
+
+    @property
+    def mix_hash(self) -> str:
+        return self._mix_hash.to_string_with_0x()
+
+    @property
+    def nonce(self) -> str:
+        return hex(self._nonce)
+
+    @property
+    def number(self) -> str:
+        return hex(self._number)
+
+    @property
+    def parent_hash(self) -> str:
+        return self._parent_hash.to_string_with_0x()
+
+    @property
+    def receipts_hash(self) -> str:
+        return self._receipts_root.to_string_with_0x()
+
+    @property
+    def uncles_hash(self) -> str:
+        return self._sha3_uncles.to_string_with_0x()
+
+    @property
+    def size(self) -> str:
+        return hex(self._size)
+
+    @property
+    def state_root(self) -> str:
+        return self._state_root.to_string_with_0x()
+
+    @property
+    def timestamp(self) -> str:
+        return hex(self._timestamp)
+
+    @property
+    def total_difficulty(self) -> str:
+        return hex(self._total_difficulty)
+
+    @property
+    def transaction_root(self) -> str:
+        return self._transactions_root.to_string_with_0x()
+
+    @property
+    def transaction(self) -> list:
+        return self._transactions
+
+    def get_transaction_by_index(self, index: int) -> EthTransaction:
+        return self._transactions[index]
+
+    @property
+    def uncles(self) -> list:
+        return self._uncles  # TODO change list of dict to object list
+
+    def get_uncles_by_index(self, index: int) -> str:
+        return self._uncles[index]
 
 
 class EthBlockTest(TestCase):
