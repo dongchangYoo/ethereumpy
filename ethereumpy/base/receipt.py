02df1403502714b6cb8fe856e417695fa9392a56
dongchangYoo <dongchang.yoo@pi-lab.io>
2021-08-20 13:46:18 +0900

    add constructor test to receipt class

--- ethereumpy/base/receipt.py
+++ ethereumpy/base/receipt.py
@@ -1,4 +1,5 @@
 import json
+from typing import Union
 from unittest import TestCase
 
 from ethereumpy.type.eth_hexstring import EthHexString, EthHashString
@@ -32,8 +33,20 @@ class Log:
         return cls(emitter, block_hash, block_number, data, log_index, removed, topics,
                    transaction_hash, transaction_index)
 
+    def to_dict(self):
+        ret_dict = dict()
+        ret_dict["address"] = self._emitter.to_string_with_0x()
+        ret_dict["blockHash"] =  self._block_hash.to_string_with_0x()
+        ret_dict["blockNumber"] = hex(self._block_number)
+        ret_dict["data"] = self._data.to_string_with_0x()
+        ret_dict["logIndex"] = hex(self._log_index)
+        ret_dict["removed"] = self.removed
+        ret_dict["topics"] = [topic.to_string_with_0x() for topic in self._topics]
+        ret_dict["transactionHash"] = self._transaction_hash.to_string_with_0x()
+        ret_dict["transactionIndex"] = hex(self._transaction_index)
+
     def serialize(self) -> bytes:
-        pass # TODO implementation
+        pass  # TODO implementation
 
     @property
     def emitter_addr(self) -> str:
@@ -77,23 +90,23 @@ class Log:
 
 class EthReceipt:
     def __init__(self, block_hash: EthHashString, block_number: int, contract_addr: ChecksumAddress,
-                 cumulative_gas_used: int, effective_gas_price: int, from_: ChecksumAddress, gas_used: int, logs: list,
-                 logs_bloom: EthHexString, status: bool, to_: ChecksumAddress, tx_hash: EthHashString, tx_index: int,
-                 type_: int):
+                 cumulative_gas_used: int, effective_gas_price: int, _from: ChecksumAddress, gas_used: int, logs: list,
+                 logs_bloom: EthHexString, status: bool, _to: ChecksumAddress, tx_hash: EthHashString, tx_index: int,
+                 _type: int):
         self._block_hash: EthHashString = block_hash
-        self.block_number: int = block_number
+        self._block_number: int = block_number
         self._contract_addr: ChecksumAddress = contract_addr
-        self.cumulative_gas_used: int = cumulative_gas_used
-        self.effective_gas_price: int = effective_gas_price
-        self.from_: ChecksumAddress = from_
-        self.gas_used: int = gas_used
+        self._cumulative_gas_used: int = cumulative_gas_used
+        self._effective_gas_price: int = effective_gas_price
+        self._from: ChecksumAddress = _from
+        self._gas_used: int = gas_used
         self._logs: list = logs
-        self.logs_bloom: EthHexString = logs_bloom
+        self._logs_bloom: EthHexString = logs_bloom
         self._status: bool = status
-        self.to_: ChecksumAddress = to_
+        self._to: ChecksumAddress = _to
         self._tx_hash: EthHashString = tx_hash
         self._tx_index: int = tx_index
-        self.type: int = type_
+        self._type: int = _type
 
     @classmethod
     def from_dict(cls, receipt_dict: dict):
@@ -101,57 +114,98 @@ class EthReceipt:
         block_number: int = int(receipt_dict["blockNumber"], 16)
         contract_addr: ChecksumAddress = ChecksumAddress(receipt_dict["contractAddress"])
         cumulative_gas_used: int = int(receipt_dict["cumulativeGasUsed"], 16)
-        effective_gas_price: int = int(receipt_dict["cumulativeGasUsed"], 16)
+        effective_gas_price: int = int(receipt_dict["effectiveGasPrice"], 16)
         sender: ChecksumAddress = ChecksumAddress(receipt_dict["from"])
         gas_used: int = int(receipt_dict["gasUsed"], 16)
-        logs: list = [Log(log) for log in receipt_dict["logs"]]
+        logs: list = [Log.from_dict(log) for log in receipt_dict["logs"]]
         logs_bloom: EthHexString = EthHexString.from_hex(receipt_dict["logsBloom"])
         status: bool = receipt_dict["status"] == "0x1"
-        to_: ChecksumAddress = ChecksumAddress(receipt_dict["to"])
+        _to: ChecksumAddress = ChecksumAddress(receipt_dict["to"])
         tx_hash: EthHashString = EthHashString.from_hex(receipt_dict["transactionHash"])
         tx_index: int = int(receipt_dict["transactionIndex"], 16)
-        type_: int = int(receipt_dict["type"], 16)
-        return cls(block_hash, block_number, contract_addr, cumulative_gas_used, effective_gas_price, sender, gas_used, logs, logs_bloom, status, to_, tx_hash, tx_index, type_)
+        _type: int = int(receipt_dict["type"], 16)
+        return cls(block_hash, block_number, contract_addr, cumulative_gas_used, effective_gas_price,
+                   sender, gas_used, logs, logs_bloom, status, _to, tx_hash, tx_index, _type)
 
-    def to_dict(self):
+    def to_dict(self) -> dict:
         ret_dict = dict()
-        ret_dict["blockHash"] = self.block_hash.to_string_with_0x()
-        ret_dict["blockNumber"] = hex(self.block_number)
-        ret_dict["contractAddress"] = self.contract_addr.to_string_with_0x()
-        ret_dict["cumulativeGasUsed"] = hex(self.cumulative_gas_used)
-        ret_dict["effectiveGasPrice"] = hex(self.effective_gas_price)
-        ret_dict["from"] = self.from_.to_string_with_0x()
-        ret_dict["gasUsed"] = hex(self.gas_used)
-
-        ret_dict["logsBloom"] = self.logs_bloom.to_string_with_0x()
-        ret_dict["status"] = "0x1" if self.status else "0x0"
-        ret_dict["to"] = self.to_.to_string_with_0x()
-        ret_dict["transactionHash"] = self.tx_hash.to_string_with_0x()
+        ret_dict["blockHash"] = self._block_hash.to_string_with_0x()
+        ret_dict["blockNumber"] = hex(self._block_number)
+        ret_dict["contractAddress"] = self._contract_addr.to_string_with_0x()
+        ret_dict["cumulativeGasUsed"] = hex(self._cumulative_gas_used)
+        ret_dict["effectiveGasPrice"] = hex(self._effective_gas_price)
+        ret_dict["from"] = self._from.to_string_with_0x()
+        ret_dict["gasUsed"] = hex(self._gas_used)
+        ret_dict["logs"] = [log.to_dict() for log in self._logs]
+        ret_dict["logsBloom"] = self._logs_bloom.to_string_with_0x()
+        ret_dict["status"] = "0x1" if self._status else "0x0"
+        ret_dict["to"] = self._to.to_string_with_0x()
+        ret_dict["transactionHash"] = self._tx_hash.to_string_with_0x()
         ret_dict["transactionIndex"] = hex(self._tx_index)
-        ret_dict["type"] = hex(self.type)
+        ret_dict["type"] = hex(self._type)
+        return ret_dict
+
+    @property
+    def block_hash(self) -> str:
+        return self._block_hash.to_string_with_0x()
+
+    @property
+    def block_number(self) -> str:
+        return hex(self._block_number)
+
+    @property
+    def contract_addr(self) -> Union[str, None]:
+        if self._contract_addr:
+            return None
+        return self._contract_addr.to_string_with_0x()
+
+    @property
+    def cumulative_gas_used(self) -> str:
+        return hex(self._cumulative_gas_used)
+
+    @property
+    def effective_gas_price(self) -> str:
+        return hex(self._effective_gas_price)
+
+    @property
+    def sender(self) -> str:
+        # TODO it may mean msg.sender?
+        return self._from.to_string_with_0x()
+
+    @property
+    def gas_used(self) -> str:
+        return hex(self._gas_used)
 
     @property
     def logs(self) -> list:
         return self._logs
 
-    @property
-    def status(self) -> bool:
-        return self._status
+    def get_log_by_index(self, index: int) -> Log:
+        return self._logs[index]
 
     @property
-    def block_hash(self) -> EthHashString:
-        return self._block_hash
+    def logs_bloom(self) -> str:
+        return self._logs_bloom.to_string_with_0x()
 
     @property
-    def contract_addr(self) -> ChecksumAddress:
-        return self._contract_addr
+    def status(self) -> str:
+        return "0x1" if self._status else "0x0"
 
     @property
-    def tx_hash(self) -> EthHashString:
-        return self._tx_hash
+    def to(self) -> str:
+        return self._to.to_string_with_0x()
 
-    def tx_idx(self) -> int:
-        return self._tx_index
+    @property
+    def transaction_hash(self) -> str:
+        return self._tx_hash.to_string_with_0x()
+
+    @property
+    def transaction_index(self) -> str:
+        return hex(self._tx_index)
+
+    @property
+    def transaction_type(self) -> str:
+        return hex(self._type)
 
 
 class ReceiptTest(TestCase):
@@ -174,5 +228,16 @@ class ReceiptTest(TestCase):
         self.assertEqual(log_dict["transactionIndex"], log_obj.transaction_index)
 
     def test_block_constructor(self):
-        tx = EthReceipt.from_dict(self.receipt_dict)
-        self.assertEqual(tx.block_hash.to_string_with_0x(), self.receipt_dict["blockHash"])
+        receipt_obj = EthReceipt.from_dict(self.receipt_dict)
+        self.assertEqual(self.receipt_dict["blockHash"], receipt_obj.block_hash)
+        self.assertEqual(self.receipt_dict["blockNumber"], receipt_obj.block_number)
+        # self.assertEqual(self.receipt_dict["contractAddress"], receipt_obj.contract_addr.lower())
+        self.assertEqual(self.receipt_dict["cumulativeGasUsed"], receipt_obj.cumulative_gas_used)
+        self.assertEqual(self.receipt_dict["effectiveGasPrice"], receipt_obj.effective_gas_price)
+        self.assertEqual(self.receipt_dict["from"], receipt_obj.sender.lower())
+        self.assertEqual(self.receipt_dict["gasUsed"], receipt_obj.gas_used)
+        self.assertEqual(self.receipt_dict["logsBloom"], receipt_obj.logs_bloom)
+        self.assertEqual(self.receipt_dict["to"], receipt_obj.to.lower())
+        self.assertEqual(self.receipt_dict["transactionHash"], receipt_obj.transaction_hash)
+        self.assertEqual(self.receipt_dict["transactionIndex"], receipt_obj.transaction_index)
+        self.assertEqual(self.receipt_dict["type"], receipt_obj.transaction_type)
